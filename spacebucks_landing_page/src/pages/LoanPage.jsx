d603772631f4bd5951da4f5749c880d2d3d4588a
PhaseOfficial <97903370+PhaseOfficial@users.noreply.github.com>
2025-03-08 01:00:54 +0200

    Update loan link in LoanPage to point to the correct URL

--- spacebucks_landing_page/src/pages/LoanPage.jsx
+++ spacebucks_landing_page/src/pages/LoanPage.jsx
@@ -89,7 +89,7 @@ rgb(3, 1, 24)
         </h1>
         
 <div class="flex flex-col bg-[#193f80] w-64 p-2 rounded-3xl gap-1">
-  <a
+  <a href="https://loans.spacebucks.co.zw/"
     class="bg-[#225bc3] hover:bg-[#2564da] hover:scale-[1.065] hover:translate-y-[-7px] hover:rounded-[23px] hover:rounded-bl-none hover:rounded-br-none px-6 py-2 rounded-2xl rounded-bl-lg rounded-br-lg transition-all text-center text-[#d8e5f9] hover:text-[#a8c1f0] font-medium cursor-pointer"
     >Individual loan
   </a>
